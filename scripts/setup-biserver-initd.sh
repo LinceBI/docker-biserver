dc88136d0c7bdf93becbd0842db56c3b9266488f
Héctor Molinero <hector.molinero@stratebi.com>
2018-09-17 12:01:13 +0200

    Posibilidad de instalar distintos plugins en cada instancia de Pentaho BI Server

--- scripts/setup-biserver-initd.sh
+++ scripts/setup-biserver-initd.sh
@@ -7,51 +7,45 @@ export LC_ALL=C
 
 ########
 
-ROOT_DIR="${BISERVER_HOME}"
 PLUGINS_DIR="${BISERVER_HOME}"/"${SOLUTIONS_DIRNAME}"/system
 
-for file in "${BISERVER_INITD}"/*; do
-	[ -e "${file}" ] || continue
-	case "${file}" in
-		# Execute shell scripts
-		*.sh|*.run)
-			logInfo "Executing script \"${file}\""
-			cd "${BISERVER_HOME}" && "${file}"
-			;;
-		# Extract zip files
-		*.zip|*.kar)
-			case "${file}" in
-				*.root.*)
-					logInfo "Uncompressing archive \"${file}\""
-					unzip -qod "${ROOT_DIR}" "${file}"
-					;;
-				*)
-					logInfo "Installing plugin \"${file}\""
-					unzip -qod "${PLUGINS_DIR}" "${file}"
-					;;
-			esac
-			;;
-		# Extract tar files
-		*.tar\
-		|*.tar.gz|*.tgz|*.taz\
-		|*.tar.bz2|*.tbz|*.tbz2|*.tz2\
-		|*.tar.lz\
-		|*.tar.lzma|*.tlz\
-		|*.tar.lzo\
-		|*.tar.xz|*.txz)
-			case "${file}" in
-				*.root.*)
-					logInfo "Uncompressing archive \"${file}\""
-					tar -C "${ROOT_DIR}" -xf "${file}"
-					;;
-				*)
-					logInfo "Installing plugin \"${file}\""
-					tar -C "${PLUGINS_DIR}" -xf "${file}"
-					;;
-			esac
-			;;
-		*)
-			logWarn "Ignoring file \"${file}\""
-			;;
-	esac
-done
+initdFromDir() {
+	directory="${1:?}"
+	for file in "${directory}"/*; do
+		[ -f "${file}" ] || continue
+		case "${file}" in
+			# Execute shell scripts
+			*.sh|*.run)
+				logInfo "Executing script \"${file}\""
+				cd "${BISERVER_HOME}" && "${file}"
+				;;
+			# Extract zip files
+			*.zip|*.kar)
+				logInfo "Installing plugin \"${file}\""
+				unzip -qod "${PLUGINS_DIR}" "${file}"
+				;;
+			# Extract tar files
+			*.tar\
+			|*.tar.gz|*.tgz|*.taz\
+			|*.tar.bz2|*.tbz|*.tbz2|*.tz2\
+			|*.tar.lz\
+			|*.tar.lzma|*.tlz\
+			|*.tar.lzo\
+			|*.tar.xz|*.txz)
+				logInfo "Installing plugin \"${file}\""
+				tar -C "${PLUGINS_DIR}" -xf "${file}"
+				;;
+			*)
+				logWarn "Ignoring file \"${file}\""
+				;;
+		esac
+	done
+}
+
+if [ -d "${BISERVER_INITD}" ]; then
+	initdFromDir "${BISERVER_INITD}"
+fi
+
+if [ -d "${BISERVER_INITD}"/"${WEBAPP_PENTAHO_DIRNAME}" ]; then
+	initdFromDir "${BISERVER_INITD}"/"${WEBAPP_PENTAHO_DIRNAME}"
+fi
