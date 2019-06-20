7c01633188694bce758f717bfbf5b499321a9c36
Héctor Molinero <hector.molinero@stratebi.com>
2019-06-20 12:36:52 +0200

    No requerir un usuario de mantenimiento de la BBDD

--- scripts/setup-biserver-storage-mysql.sh
+++ scripts/setup-biserver-storage-mysql.sh
@@ -8,15 +8,16 @@ export LC_ALL=C
 ########
 
 mysqlRun() {
-	MYSQL_PWD="${MYSQL_PASSWORD}" mysql \
+	database=${1:?}; username=${2:?}; password=${3:?}; shift 3
+	MYSQL_PWD="${password}" mysql \
 		--host="${MYSQL_HOST}" \
 		--port="${MYSQL_PORT}" \
-		--user="${MYSQL_USER}" \
-		"$@"
+		--user="${username}" \
+		"${database}" "$@"
 }
 
-mysqlDbExists() {
-	mysqlRun -NBe 'SHOW DATABASES' | grep -qwi -- "$1"
+mysqlConnect() {
+	mysqlRun "${1:?}" "${2:?}" "${3:?}" -e '\q' >/dev/null 2>&1
 }
 
 ########
@@ -24,30 +25,38 @@ mysqlDbExists() {
 if [ "${EXPORT_ENABLED}" = 'false' ]; then
 	logInfo 'Checking MySQL connection...'
 	CONNECTION_RETRIES=0; MAX_CONNECTION_RETRIES=60
-	until mysqlRun -e '\q'; do
+	until
+		mysqlConnect "${MYSQL_MAINTENANCE_DATABASE}" "${MYSQL_USER}" "${MYSQL_PASSWORD}" ||
+		mysqlConnect "${MYSQL_JACKRABBIT_DATABASE}" "${MYSQL_JACKRABBIT_USER}" "${MYSQL_JACKRABBIT_PASSWORD}" ||
+		mysqlConnect "${MYSQL_HIBERNATE_DATABASE}" "${MYSQL_HIBERNATE_USER}" "${MYSQL_HIBERNATE_PASSWORD}" ||
+		mysqlConnect "${MYSQL_QUARTZ_DATABASE}" "${MYSQL_QUARTZ_USER}" "${MYSQL_QUARTZ_PASSWORD}"
+	do
 		if [ "${CONNECTION_RETRIES}" -gt "${MAX_CONNECTION_RETRIES}" ]; then
 			logFail 'MySQL connection failed'
 			exit 1
 		fi
-		CONNECTION_RETRIES=$(( CONNECTION_RETRIES + 1 ))
+		CONNECTION_RETRIES=$((CONNECTION_RETRIES + 1))
 		sleep 1
 	done
 
 	logInfo "Checking \"${MYSQL_JACKRABBIT_DATABASE}\" database..."
-	if ! mysqlDbExists "${MYSQL_JACKRABBIT_DATABASE}"; then
+	if ! mysqlConnect "${MYSQL_JACKRABBIT_DATABASE}" "${MYSQL_JACKRABBIT_USER}" "${MYSQL_JACKRABBIT_PASSWORD}"; then
 		logInfo "Creating \"${MYSQL_JACKRABBIT_DATABASE}\" database..."
-		mysqlRun < "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_jcr_mysql.sql
+		mysqlRun "${MYSQL_MAINTENANCE_DATABASE}" "${MYSQL_USER}" "${MYSQL_PASSWORD}" \
+			< "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_jcr_mysql.sql
 	fi
 
 	logInfo "Checking \"${MYSQL_HIBERNATE_DATABASE}\" database..."
-	if ! mysqlDbExists "${MYSQL_HIBERNATE_DATABASE}"; then
+	if ! mysqlConnect "${MYSQL_HIBERNATE_DATABASE}" "${MYSQL_HIBERNATE_USER}" "${MYSQL_HIBERNATE_PASSWORD}"; then
 		logInfo "Creating \"${MYSQL_HIBERNATE_DATABASE}\" database..."
-		mysqlRun < "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_repository_mysql.sql
+		mysqlRun "${MYSQL_MAINTENANCE_DATABASE}" "${MYSQL_USER}" "${MYSQL_PASSWORD}" \
+			< "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_repository_mysql.sql
 	fi
 
 	logInfo "Checking \"${MYSQL_QUARTZ_DATABASE}\" database..."
-	if ! mysqlDbExists "${MYSQL_QUARTZ_DATABASE}"; then
+	if ! mysqlConnect "${MYSQL_QUARTZ_DATABASE}" "${MYSQL_QUARTZ_USER}" "${MYSQL_QUARTZ_PASSWORD}"; then
 		logInfo "Creating \"${MYSQL_QUARTZ_DATABASE}\" database..."
-		mysqlRun < "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_quartz_mysql.sql
+		mysqlRun "${MYSQL_MAINTENANCE_DATABASE}" "${MYSQL_USER}" "${MYSQL_PASSWORD}" \
+			< "${BISERVER_HOME}"/"${DATA_DIRNAME}"/mysql5/create_quartz_mysql.sql
 	fi
 fi
--- scripts/setup-biserver-storage-postgres.sh
+++ scripts/setup-biserver-storage-postgres.sh
@@ -8,16 +8,17 @@ export LC_ALL=C
 ########
 
 psqlRun() {
-	PGPASSWORD="${POSTGRES_PASSWORD}" psql \
+	database=${1:?}; username=${2:?}; password=${3:?}; shift 3
+	PGPASSWORD="${password}" psql \
 		--host="${POSTGRES_HOST}" \
 		--port="${POSTGRES_PORT}" \
-		--username="${POSTGRES_USER}" \
-		--dbname="${POSTGRES_MAINTENANCE_DATABASE}" \
+		--username="${username}" \
+		--dbname="${database}" \
 		"$@"
 }
 
-psqlDbExists() {
-	psqlRun -lqt | cut -d'|' -f1 | grep -qwi -- "$1"
+psqlConnect() {
+	psqlRun "${1:?}" "${2:?}" "${3:?}" -c '\conninfo' >/dev/null 2>&1
 }
 
 ########
@@ -25,30 +26,38 @@ psqlDbExists() {
 if [ "${EXPORT_ENABLED}" = 'false' ]; then
 	logInfo 'Checking PostgreSQL connection...'
 	CONNECTION_RETRIES=0; MAX_CONNECTION_RETRIES=60
-	until psqlRun -c '\conninfo'; do
+	until
+		psqlConnect "${POSTGRES_MAINTENANCE_DATABASE}" "${POSTGRES_USER}" "${POSTGRES_PASSWORD}" ||
+		psqlConnect "${POSTGRES_JACKRABBIT_DATABASE}" "${POSTGRES_JACKRABBIT_USER}" "${POSTGRES_JACKRABBIT_PASSWORD}" ||
+		psqlConnect "${POSTGRES_HIBERNATE_DATABASE}" "${POSTGRES_HIBERNATE_USER}" "${POSTGRES_HIBERNATE_PASSWORD}" ||
+		psqlConnect "${POSTGRES_QUARTZ_DATABASE}" "${POSTGRES_QUARTZ_USER}" "${POSTGRES_QUARTZ_PASSWORD}"
+	do
 		if [ "${CONNECTION_RETRIES}" -gt "${MAX_CONNECTION_RETRIES}" ]; then
 			logFail 'PostgreSQL connection failed'
 			exit 1
 		fi
-		CONNECTION_RETRIES=$(( CONNECTION_RETRIES + 1 ))
+		CONNECTION_RETRIES=$((CONNECTION_RETRIES + 1))
 		sleep 1
 	done
 
 	logInfo "Checking \"${POSTGRES_JACKRABBIT_DATABASE}\" database..."
-	if ! psqlDbExists "${POSTGRES_JACKRABBIT_DATABASE}"; then
+	if ! psqlConnect "${POSTGRES_JACKRABBIT_DATABASE}" "${POSTGRES_JACKRABBIT_USER}" "${POSTGRES_JACKRABBIT_PASSWORD}"; then
 		logInfo "Creating \"${POSTGRES_JACKRABBIT_DATABASE}\" database..."
-		psqlRun -f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_jcr_postgresql.sql
+		psqlRun "${POSTGRES_MAINTENANCE_DATABASE}" "${POSTGRES_USER}" "${POSTGRES_PASSWORD}" \
+			-f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_jcr_postgresql.sql
 	fi
 
 	logInfo "Checking \"${POSTGRES_HIBERNATE_DATABASE}\" database..."
-	if ! psqlDbExists "${POSTGRES_HIBERNATE_DATABASE}"; then
+	if ! psqlConnect "${POSTGRES_HIBERNATE_DATABASE}" "${POSTGRES_HIBERNATE_USER}" "${POSTGRES_HIBERNATE_PASSWORD}"; then
 		logInfo "Creating \"${POSTGRES_HIBERNATE_DATABASE}\" database..."
-		psqlRun -f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_repository_postgresql.sql
+		psqlRun "${POSTGRES_MAINTENANCE_DATABASE}" "${POSTGRES_USER}" "${POSTGRES_PASSWORD}" \
+			-f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_repository_postgresql.sql
 	fi
 
 	logInfo "Checking \"${POSTGRES_QUARTZ_DATABASE}\" database..."
-	if ! psqlDbExists "${POSTGRES_QUARTZ_DATABASE}"; then
+	if ! psqlConnect "${POSTGRES_QUARTZ_DATABASE}" "${POSTGRES_QUARTZ_USER}" "${POSTGRES_QUARTZ_PASSWORD}"; then
 		logInfo "Creating \"${POSTGRES_QUARTZ_DATABASE}\" database..."
-		psqlRun -f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_quartz_postgresql.sql
+		psqlRun "${POSTGRES_MAINTENANCE_DATABASE}" "${POSTGRES_USER}" "${POSTGRES_PASSWORD}" \
+			-f "${BISERVER_HOME}"/"${DATA_DIRNAME}"/postgresql/create_quartz_postgresql.sql
 	fi
 fi
