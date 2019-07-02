b5048021fd018f895f964bcd3099c9d85b2b73b5
Héctor <hector.molinero@stratebi.com>
2019-07-02 08:27:35 +0200

    Evitar error si los métodos quoteRe y quoteSubst no reciben un argumento

--- scripts/set-utils.sh
+++ scripts/set-utils.sh
@@ -5,8 +5,8 @@ export LC_ALL=C
 
 # Escape strings in sed
 # See: https://stackoverflow.com/a/29613573
-quoteRe() { printf -- '%s' "${1:?}" | sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a'\\''"$(printf '\n')"'\\n' | tr -d '\n'; }
-quoteSubst() { printf -- '%s' "${1:?}" | sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g'; }
+quoteRe() { printf -- '%s' "${1-}" | sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a'\\''"$(printf '\n')"'\\n' | tr -d '\n'; }
+quoteSubst() { printf -- '%s' "${1-}" | sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g'; }
 
 # Check if a string matches a pattern
 matches() { printf -- '%s' "${1:?}" | grep -q "${2:?}"; }
