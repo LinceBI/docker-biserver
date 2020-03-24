1df46df2bdf7008dbc10ebd3f858bd2fe181772b
Héctor <hector.molinero@stratebi.com>
2020-03-24 15:42:21 +0100

    Establecer permisos en un script separado

---
+++ scripts/bin/update-permissions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eu
+export LC_ALL=C
+
+# shellcheck disable=SC1091
+. /usr/share/biserver/bin/set-utils.sh
+
+########
+
+find "${BISERVER_HOME:?}" "${CATALINA_HOME:?}" "${CATALINA_BASE:?}" -type d -not -perm 0775 -exec chmod -c 0775 '{}' '+'
+find "${BISERVER_HOME:?}" "${CATALINA_HOME:?}" "${CATALINA_BASE:?}" -type f -not '(' -perm 0664 -o -regex '.*\.sh\(\.erb\)?$' ')' -exec chmod -c 0664 '{}' '+'
+find "${BISERVER_HOME:?}" "${CATALINA_HOME:?}" "${CATALINA_BASE:?}" -type f '(' -not -perm 0775 -a -regex '.*\.sh\(\.erb\)?$' ')' -exec chmod -c 0775 '{}' '+'
+
+find "${BISERVER_INITD:?}" /home/biserver/ -type d -not -perm 0775 -exec chmod -c 0775 '{}' '+'
+find "${BISERVER_INITD:?}" /home/biserver/ -type f -not '(' -perm 0664 -o -regex '.*\.\(sh\|run\)$' ')' -exec chmod -c 0664 '{}' '+'
+find "${BISERVER_INITD:?}" /home/biserver/ -type f '(' -not -perm 0775 -a -regex '.*\.\(sh\|run\)$' ')' -exec chmod -c 0775 '{}' '+'
+
+find /usr/share/biserver/bin/ /usr/share/biserver/service/ -not -perm 0775 -exec chmod -c 0775 '{}' '+'
