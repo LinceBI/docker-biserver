4e1a20fd31f417b36fe987d9397d7aec11be133a
Héctor <hector.molinero@stratebi.com>
2020-03-27 14:47:37 +0100

    Realizar setup de BI Server solo si el servicio está habilitado

--- scripts/bin/init.sh
+++ scripts/bin/init.sh
@@ -8,17 +8,15 @@ export LC_ALL=C
 
 ########
 
-BISERVER_SETUP_LCK_FILE="${BISERVER_HOME:?}"/setup.lock
-BISERVER_SETUP_LOG_FILE="${CATALINA_BASE:?}"/logs/setup.log
-if [ ! -f "${BISERVER_SETUP_LCK_FILE:?}" ]; then
-	runAndLog /usr/share/biserver/bin/setup.sh "${BISERVER_SETUP_LOG_FILE}"
-	touch "${BISERVER_SETUP_LCK_FILE:?}"
-fi
-
-########
-
 if [ "${SERVICE_BISERVER_ENABLED:?}" = 'true' ]; then
 	runitEnSv biserver
+
+	BISERVER_SETUP_LCK_FILE="${BISERVER_HOME:?}"/setup.lock
+	BISERVER_SETUP_LOG_FILE="${CATALINA_BASE:?}"/logs/setup.log
+	if [ ! -f "${BISERVER_SETUP_LCK_FILE:?}" ]; then
+		runAndLog /usr/share/biserver/bin/setup.sh "${BISERVER_SETUP_LOG_FILE}"
+		touch "${BISERVER_SETUP_LCK_FILE:?}"
+	fi
 fi
 
 if [ "${SERVICE_SUPERCRONIC_ENABLED:?}" = 'true' ] && [ -e "${BIUSER_HOME:?}"/crontab ]; then
