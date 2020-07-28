9dc71741d28c8240e417278c337f86dde3fb1bf4
Héctor <hector.molinero@stratebi.com>
2020-07-28 09:37:43 +0200

    Continuar aunque la limpieza de archivos temporales falle

--- scripts/bin/set-utils.sh
+++ scripts/bin/set-utils.sh
@@ -8,7 +8,7 @@ export UMASK=0002
 umask "${UMASK:?}"
 
 # Clean up temp directory on exit
-trap 'find "${TMPDIR:-/tmp}" -mindepth 1 -writable -delete; trap - EXIT' EXIT TERM INT HUP
+trap 'find "${TMPDIR:-/tmp}" -mindepth 1 -writable -delete ||:; trap - EXIT' EXIT TERM INT HUP
 
 # Some regex patterns
 export PATTERN_EXT_RUN="\.\(sh\|run\)$"
