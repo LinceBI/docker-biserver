92f403456f0b6f7d97877eae8f9b9d548af4ed98
Héctor <hector.molinero@stratebi.com>
2021-09-07 15:40:56 +0200

    Actualización a Pentaho 9.2.0.0-290

--- examples/export-with-mysql.sh
+++ examples/export-with-mysql.sh
@@ -8,7 +8,7 @@ DOCKER=$(command -v docker 2>/dev/null)
 IMAGE_REGISTRY=repo.stratebi.com
 IMAGE_NAMESPACE=lincebi
 IMAGE_PROJECT=biserver
-IMAGE_TAG=9.1.0.0-324
+IMAGE_TAG=9.2.0.0-290
 IMAGE_NAME=${IMAGE_REGISTRY:?}/${IMAGE_NAMESPACE:?}/${IMAGE_PROJECT:?}:${IMAGE_TAG:?}
 
 exec "${DOCKER:?}" run --rm --log-driver none --attach STDOUT --attach STDERR \
