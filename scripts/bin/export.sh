646da72051a05dfc67235fc0dc3bcb06fd101855
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2022-04-07 16:27:10 +0200

    Cargar set-utils.sh en todos los scripts de bin

--- scripts/bin/export.sh
+++ scripts/bin/export.sh
@@ -3,6 +3,11 @@
 set -eu
 export LC_ALL=C
 
+# shellcheck source=./set-utils.sh
+. /usr/share/biserver/bin/set-utils.sh
+
+########
+
 # Execute setup scripts
 export IS_EXPORTING='true'
 /usr/share/biserver/bin/setup.sh 1>&2
