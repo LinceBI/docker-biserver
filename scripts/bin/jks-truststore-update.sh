09e6df9008faf4a9a4d1e6d8493f4b8ac3a0298f
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2022-11-04 13:34:52 +0100

    Añadido soporte para añadir certificados al contenedor

---
+++ scripts/bin/jks-truststore-update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+export LC_ALL=C
+
+# shellcheck source=./set-utils.sh
+. /usr/share/biserver/bin/set-utils.sh
+
+########
+
+mkdir -p "$(dirname "${JAVA_TRUSTSTORE_FILE:?}")"
+trust extract --overwrite --format=java-cacerts --filter=ca-anchors --purpose=server-auth "${JAVA_TRUSTSTORE_FILE:?}"
