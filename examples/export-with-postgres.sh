8805a8cebb115cb241f30b3de11e00e173a5070b
Héctor Molinero Fernández <hector.molinero@stratebi.com>
2024-07-16 11:41:28 +0200

    Incremento de versión

--- examples/export-with-postgres.sh
+++ examples/export-with-postgres.sh
@@ -8,7 +8,7 @@ DOCKER=$(command -v docker 2>/dev/null)
 IMAGE_REGISTRY=repo.stratebi.com
 IMAGE_NAMESPACE=lincebi
 IMAGE_PROJECT=biserver
-IMAGE_TAG=9.5.2.0-272-3
+IMAGE_TAG=9.5.2.0-272-4
 IMAGE_NAME=${IMAGE_REGISTRY:?}/${IMAGE_NAMESPACE:?}/${IMAGE_PROJECT:?}:${IMAGE_TAG:?}
 
 exec "${DOCKER:?}" run --rm --log-driver none --attach STDOUT --attach STDERR \
